e9db78931aac8bc636401d938bab21e30a04b7f3
kimjinsu <creeeeeeeeeeeeeping@gmail.com>
2026-01-20 18:19:46 +0900

    ✨ feat     : 유저 모델 작성

--- apps/core/models.py
+++ apps/core/models.py
@@ -5,6 +5,5 @@ class TimeStampModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-
     class Meta:
-        abstract = True# Create your models here.
+        abstract = True  # Create your models here.
