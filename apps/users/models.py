b3856b285aacc598f0e2c19b1cfa2b8b1454551a
kimjinsu <creeeeeeeeeeeeeping@gmail.com>
2026-01-20 17:38:07 +0900

    ✨ feat     : 유저 모델 작성

--- apps/users/models.py
+++ apps/users/models.py
@@ -2,13 +2,7 @@ from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
-
-class TimeStampModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        abstract = True
+from apps.core.models import TimeStampModel
 
 
 class UserManager(BaseUserManager):
