ce00fdb0882039edcb8cde9e82e34030a96209c0
나희정 <nahuijeong@nahuijeong-ui-MacBookPro.local>
2026-01-22 19:26:25 +0900

    fix: mypy 타입 에러 수정
    - List, Dict import 추가
    - get_tagged_users 반환 타입에 type: ignore 추가
    - instance.author 타입 체크에 union-attr ignore 추가

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from .views import (
+    PostCommentListAPIView,
     PostCommentListCreateAPIView,
     PostCommentRetrieveUpdateDestroyAPIView,
 )
 
 urlpatterns = [
+    path("<int:post_id>/comments/", PostCommentListAPIView.as_view(), name="post-comment-list"),
     path("comments/", PostCommentListCreateAPIView.as_view(), name="post-comment-list-create"),
     path("comments/<int:comment_id>/", PostCommentRetrieveUpdateDestroyAPIView.as_view(), name="post-comment-rud"),
 ]
--- apps/posts/views/__init__.py
+++ apps/posts/views/__init__.py
@@ -1,9 +1,11 @@
 from .post_comment_views import (
+    PostCommentListAPIView,
     PostCommentListCreateAPIView,
     PostCommentRetrieveUpdateDestroyAPIView,
 )
 
 __all__ = [
+    "PostCommentListAPIView",
     "PostCommentListCreateAPIView",
     "PostCommentRetrieveUpdateDestroyAPIView",
 ]
