6dbd68b5f9ce965c9c4d426f7980f31da0cda5c9
나희정 <nahuijeong@nahuijeong-ui-MacBookPro.local>
2026-01-22 15:05:03 +0900

    WIP: save local changes before pulling

--- apps/posts/serializers/__init__.py
+++ apps/posts/serializers/__init__.py
@@ -1,5 +1,28 @@
+<<<<<<< HEAD
 from .post_category import PostCategorySerializer
+from .post_comment import (
+    PostCommentCreateSerializer,
+    PostCommentDeleteResponseSerializer,
+    PostCommentListSerializer,
+    PostCommentUpdateSerializer,
+)
+from .post_serializers import (
+    DeleteResponseSerializer,
+    PostCreateSerializer,
+    PostDetailSerializer,
+    PostListSerializer,
+    PostUpdateSerializer,
+)
 
 __all__ = [
     "PostCategorySerializer",
+    "PostCreateSerializer",
+    "PostListSerializer",
+    "PostDetailSerializer",
+    "PostUpdateSerializer",
+    "DeleteResponseSerializer",
+    "PostCommentListSerializer",
+    "PostCommentCreateSerializer",
+    "PostCommentUpdateSerializer",
+    "PostCommentDeleteResponseSerializer",
 ]
