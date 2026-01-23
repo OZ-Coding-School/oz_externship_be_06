333b80b42297f80040f6cee65e10e2fb6ec0cf7b
dskim6797 <dskim6797@gmail.com>
2026-01-23 15:10:08 +0900

    💡 chore: image_ids 변수 및 로직 제거
    - PR 리뷰 및 API 명세서 참고하여 수정
    - 이미지는 url을 content 담아서 response
    - 추후 content에서 파싱해서 이미지 관련 로직 구동하도록 구현

--- apps/qna/serializers/question_serializer.py
+++ apps/qna/serializers/question_serializer.py
@@ -8,13 +8,10 @@ from apps.qna.serializers.question_base import QnAVersionedValidationMixin
 
 class QuestionCreateSerializer(QnAVersionedValidationMixin, serializers.ModelSerializer[Question]):
     category_id = serializers.IntegerField(required=True, help_text="카테고리 ID (소분류)")
-    image_ids = serializers.ListField(
-        child=serializers.IntegerField(), required=False, write_only=True, help_text="첨부할 이미지 PK 리스트"
-    )
 
     class Meta:
         model = Question
-        fields = ["title", "content", "category_id", "image_ids"]
+        fields = ["title", "content", "category_id"]
 
 
 class QuestionCreateResponseSerializer(QnAVersionedValidationMixin, serializers.Serializer[Any]):
--- apps/qna/services/question_service.py
+++ apps/qna/services/question_service.py
@@ -3,7 +3,7 @@ from typing import Any
 from django.db import transaction
 
 from apps.qna.exceptions.question_exception import QuestionBaseException
-from apps.qna.models import Question, QuestionCategory, QuestionImage
+from apps.qna.models import Question, QuestionCategory
 
 
 class QuestionService:
@@ -11,21 +11,16 @@ class QuestionService:
     @transaction.atomic
     def create_question(author: Any, data: dict[str, Any]) -> Question:
         """
-        질문 생성 및 리소스 연결 로직
+        질문 생성 로직
         """
         try:
             category_id = data.pop("category_id")
-            image_ids = data.pop("image_ids", [])
 
             # 카테고리 획득
             category = QuestionCategory.objects.get(id=category_id)
 
             # 질문 생성
             question = Question.objects.create(author=author, category=category, **data)
-
-            # 이미지 연관 관계 업데이트
-            if image_ids:
-                QuestionImage.objects.filter(id__in=image_ids, question__isnull=True).update(question=question)
 
             return question
 
