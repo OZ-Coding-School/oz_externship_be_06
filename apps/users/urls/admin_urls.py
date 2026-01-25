0f1d9117b14153fa53556cf44e8c09a349339ed6
zodongchan <ejsvkgkarj@gmail.com>
2026-01-25 22:45:03 +0900

    ♻️ refactor: 어드민 urls 리팩터링
    # Conflicts:
    #	apps/users/admin_urls.py

--- apps/users/urls/admin_urls.py
+++
@@ -1,9 +0,0 @@
-# apps/users/urls/admin_urls.py
-from django.urls import path
-
-from apps.users.views.admin.account_views import AdminUserListView
-
-urlpatterns = [
-    # api/v1/admin/accounts 요청이 들어오면 바로 이 View로 연결됨
-    path("", AdminUserListView.as_view(), name="admin-account-list"),
-]
