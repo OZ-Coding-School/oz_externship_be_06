18d64d556b42c47843ab2ed81b0316464ed7cd6d
ds8000 <ds8600@naver.com>
2026-01-26 11:34:47 +0900

    chore: fix formatting and mypy for withdrawal api

--- apps/users/views/withdrawal_view.py
+++ apps/users/views/withdrawal_view.py
@@ -1,12 +1,14 @@
 from django.db import transaction
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.views import APIView
+from drf_spectacular.utils import extend_schema
 from rest_framework import status
-from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from apps.users.models import Withdrawal
 from apps.users.serializers import WithdrawalRequestSerializer
-from drf_spectacular.utils import extend_schema
+
 
 class WithdrawalAPIView(APIView):
     permission_classes = [IsAuthenticated]
@@ -29,14 +31,14 @@ class WithdrawalAPIView(APIView):
         existing = Withdrawal.objects.filter(user=user).first()
         if existing:
             return Response(
-            {
-                "error_detail": {
-                    "detail": "이미 탈퇴 신청한 계정입니다.",
-                    "expire_at": existing.due_date,
-                }
-            },
-            status=status.HTTP_409_CONFLICT,
-        )
+                {
+                    "error_detail": {
+                        "detail": "이미 탈퇴 신청한 계정입니다.",
+                        "expire_at": existing.due_date,
+                    }
+                },
+                status=status.HTTP_409_CONFLICT,
+            )
 
         serializer = WithdrawalRequestSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
