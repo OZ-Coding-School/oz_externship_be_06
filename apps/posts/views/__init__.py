21101fe76e5250b8d8c3966e96c18c4e58861a79
nahuijeong <hwayuya@gmail.com>
2026-01-26 14:57:45 +0900

    🐛 fix      : init 파일 수정

--- apps/posts/views/__init__.py
+++ apps/posts/views/__init__.py
@@ -1,12 +1,5 @@
 from .post_category_views import PostCategoryListAPIView
-from .post_comment_views import (
-    PostCommentListCreateAPIView,
-    PostCommentRetrieveUpdateDestroyAPIView,
-)
-from .post_category_views import PostCategoryListAPIView
 
 __all__ = [
     "PostCategoryListAPIView",
-    "PostCommentListCreateAPIView",
-    "PostCommentRetrieveUpdateDestroyAPIView",
 ]
