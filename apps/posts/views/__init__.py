8161caa8793404a1e6907d23d160b6642682803b
나희정 <nahuijeong@nahuijeong-ui-MacBookPro.local>
2026-01-26 13:33:17 +0900

    feat(posts): 게시글 카테고리 목록 조회 추가
    - PostCategorySerializer(id, name) 추가
    
    - 카테고리 목록 조회 API 뷰 추가
    
    - 관련 테스트 추가

--- apps/posts/views/__init__.py
+++ apps/posts/views/__init__.py
@@ -6,6 +6,7 @@ from .post_comment_views import (
     PostCommentListCreateAPIView,
     PostCommentRetrieveUpdateDestroyAPIView,
 )
+from .post_category_views import PostCategoryListAPIView
 
 __all__ = [
 <<<<<<< HEAD
