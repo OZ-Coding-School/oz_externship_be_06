53349fc2dd5d7277759e5281e8533da6db3dc0bd
nahuijeong <hwayuya@gmail.com>
2026-01-26 14:57:45 +0900

    🐛 fix      : init 파일 수정

--- apps/posts/serializers/__init__.py
+++ apps/posts/serializers/__init__.py
@@ -1,34 +1,5 @@
-<<<<<<< HEAD
-=======
 from .post_category import PostCategorySerializer
->>>>>>> 23b79fe (chore(posts): isort 통과를 위한 import 정렬)
-from .post_comment import (
-    PostCommentCreateSerializer,
-    PostCommentDeleteResponseSerializer,
-    PostCommentListSerializer,
-    PostCommentUpdateSerializer,
-)
-from .post_category import PostCategorySerializer
-from .post_serializers import (
-    DeleteResponseSerializer,
-    PostCreateSerializer,
-    PostDetailSerializer,
-    PostListSerializer,
-    PostUpdateSerializer,
-)
 
 __all__ = [
-<<<<<<< HEAD
-=======
     "PostCategorySerializer",
->>>>>>> 23b79fe (chore(posts): isort 통과를 위한 import 정렬)
-    "PostCreateSerializer",
-    "PostListSerializer",
-    "PostDetailSerializer",
-    "PostUpdateSerializer",
-    "DeleteResponseSerializer",
-    "PostCommentListSerializer",
-    "PostCommentCreateSerializer",
-    "PostCommentUpdateSerializer",
-    "PostCommentDeleteResponseSerializer",
 ]
--- apps/posts/views/__init__.py
+++ apps/posts/views/__init__.py
@@ -1,18 +1,5 @@
-<<<<<<< HEAD
-=======
-from .post_category_views import PostCategoryListAPIView
->>>>>>> 23b79fe (chore(posts): isort 통과를 위한 import 정렬)
-from .post_comment_views import (
-    PostCommentListCreateAPIView,
-    PostCommentRetrieveUpdateDestroyAPIView,
-)
 from .post_category_views import PostCategoryListAPIView
 
 __all__ = [
-<<<<<<< HEAD
-=======
     "PostCategoryListAPIView",
->>>>>>> 23b79fe (chore(posts): isort 통과를 위한 import 정렬)
-    "PostCommentListCreateAPIView",
-    "PostCommentRetrieveUpdateDestroyAPIView",
 ]
