9cadde647b96aed447c515e836337a9de35cf66d
나희정 <nahuijeong@nahuijeong-ui-MacBookPro.local>
2026-01-26 13:45:35 +0900

    chore(posts): isort 통과를 위한 import 정렬
    - serializers/__init__.py, views/__init__.py에서 상대 import 순서를 isort 규칙에 맞게 정리

--- apps/posts/serializers/__init__.py
+++ apps/posts/serializers/__init__.py
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
+=======
+from .post_category import PostCategorySerializer
+>>>>>>> 23b79fe (chore(posts): isort 통과를 위한 import 정렬)
 from .post_comment import (
     PostCommentCreateSerializer,
     PostCommentDeleteResponseSerializer,
@@ -13,6 +17,10 @@ from .post_serializers import (
 )
 
 __all__ = [
+<<<<<<< HEAD
+=======
+    "PostCategorySerializer",
+>>>>>>> 23b79fe (chore(posts): isort 통과를 위한 import 정렬)
     "PostCreateSerializer",
     "PostListSerializer",
     "PostDetailSerializer",
--- apps/posts/views/__init__.py
+++ apps/posts/views/__init__.py
@@ -1,9 +1,17 @@
+<<<<<<< HEAD
+=======
+from .post_category_views import PostCategoryListAPIView
+>>>>>>> 23b79fe (chore(posts): isort 통과를 위한 import 정렬)
 from .post_comment_views import (
     PostCommentListCreateAPIView,
     PostCommentRetrieveUpdateDestroyAPIView,
 )
 
 __all__ = [
+<<<<<<< HEAD
+=======
+    "PostCategoryListAPIView",
+>>>>>>> 23b79fe (chore(posts): isort 통과를 위한 import 정렬)
     "PostCommentListCreateAPIView",
     "PostCommentRetrieveUpdateDestroyAPIView",
 ]
