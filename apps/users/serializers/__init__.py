719a5eb617110768d4246cb2f825a7d4345bdfbb
ds8000 <ds8600@naver.com>
2026-01-26 11:34:47 +0900

    chore: fix formatting and mypy for withdrawal api

--- apps/users/serializers/__init__.py
+++ apps/users/serializers/__init__.py
@@ -4,10 +4,11 @@ from .email_verification_serializer import (
 )
 from .sign_up_serializer import SignupNicknameCheckSerializer, SignUpSerializer
 from .withdrawal_serializer import WithdrawalRequestSerializer
+
 __all__ = [
     "SignUpSerializer",
     "SignupNicknameCheckSerializer",
     "SendEmailVerificationSerializer",
     "VerifyEmailSerializer",
-    "WithdrawalRequestSerializer"
+    "WithdrawalRequestSerializer",
 ]
--- apps/users/serializers/withdrawal_serializer.py
+++ apps/users/serializers/withdrawal_serializer.py
@@ -3,6 +3,6 @@ from rest_framework import serializers
 from apps.users.models.withdrawal import Withdrawal
 
 
-class WithdrawalRequestSerializer(serializers.Serializer):
-    reason = serializers.ChoiceField(choices=Withdrawal.Reason.choices) # type: ignore[arg-type]
+class WithdrawalRequestSerializer(serializers.Serializer):  # type: ignore[type-arg]
+    reason = serializers.ChoiceField(choices=Withdrawal.Reason.choices)
     reason_detail = serializers.CharField(allow_blank=False, trim_whitespace=True)
