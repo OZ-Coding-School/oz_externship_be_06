7ba19b46230be8f9f924e597aae72f7b19d4f531
jhjdl3984 <jhjdl3984@gmail.com>
2026-01-27 11:23:41 +0900

    - 💡 chore    : 어드민수정 api 구현중

--- apps/exams/serializers/admin_exam_update.py
+++ apps/exams/serializers/admin_exam_update.py
@@ -5,13 +5,10 @@ class AdminExamUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = ExamDeployment
         fields = ['title', 'subject_id', 'thumbnail_img']
-        extra_kwargs = {
-            'title': {'required': True},
-            'subject_id': {'required': True},
-        }
 
+    # 409
     def validate_title(self, value):
         # 이름 중복 체크 예시
-        if ExamDeployment.objects.filter(title=value).exclude(id=self.instance.id if self.instance else None).exists():
+        if ExamDeployment.objects.filter(title=value).exclude(id=self.instance.id).exists():
             raise serializers.ValidationError("동일한 이름의 쪽지시험이 이미 존재합니다.")
         return value
