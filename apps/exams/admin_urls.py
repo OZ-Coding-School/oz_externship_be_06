e606e5773c6420fff5b4a6452c29c427b3c2a99e
god-gy <god.gy0321@gmail.com>
2026-01-27 10:13:37 +0900

    ♻️ refactor: admin exams 라우팅 중복 제거
    - 문제 등록 엔드포인트 중복 정의 제거

--- apps/exams/admin_urls.py
+++ apps/exams/admin_urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
 from apps.exams.views.admin_exam_delete_views import AdminExamDeleteAPIView
-from apps.exams.views.admin_question_delete_views import AdminExamQuestionDeleteAPIView
 from apps.exams.views.admin_exam_views import AdminExamCreateAPIView
+from apps.exams.views.admin_question_delete_views import AdminExamQuestionDeleteAPIView
 from apps.exams.views.admin_question_views import AdminExamQuestionCreateAPIView
 from apps.exams.views.admin_submission_views import AdminExamSubmissionListAPIView
 
@@ -11,7 +11,6 @@ urlpatterns = [
     path("exams/<int:exam_id>/", AdminExamDeleteAPIView.as_view(), name="admin-exam-delete"),
     path("exams", AdminExamCreateAPIView.as_view(), name="admin-exam-create"),
     path("submissions/", AdminExamSubmissionListAPIView.as_view(), name="admin-exam-submission-list"),
-    path("exams/<int:exam_id>/questions/", AdminExamQuestionCreateAPIView.as_view(), name="admin-exam-question-create"),
     path(
         "exams/questions/<int:question_id>/",
         AdminExamQuestionDeleteAPIView.as_view(),
