d39e670e8ac2b01bb2d43ff2c9028c78121764e9
huijeong99 <hwayuya@gmail.com>
2026-01-28 14:03:03 +0900

    Fix: update import path for PostCategoryListAPIView in posts/urls.py to resolve black parse error and ensure consistency
    # Conflicts:
    #	apps/posts/urls.py

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -4,11 +4,30 @@ from apps.posts.views.category_views import CategoryListView
 from apps.posts.views.post_like_views import PostLikeAPIView
 from apps.posts.views.post_views import PostDetailView, PostListCreateView
 
+from .views import (
+    PostCategoryListAPIView,
+    PostCommentListCreateAPIView,
+    PostCommentRetrieveUpdateDestroyAPIView,
+)
+
 app_name = "posts"
 
 urlpatterns = [
     path("", PostListCreateView.as_view(), name="post-list-create"),
     path("<int:post_id>", PostDetailView.as_view(), name="post-detail"),
     path("categories", CategoryListView.as_view(), name="category-list"),
+    # 댓글 목록 / 생성
+    path(
+        "<int:post_id>/comments/",
+        PostCommentListCreateAPIView.as_view(),
+        name="post-comment-list-create",
+    ),
+    # 댓글 상세 / 수정 / 삭제
+    path(
+        "<int:post_id>/comments/<int:comment_id>/",
+        PostCommentRetrieveUpdateDestroyAPIView.as_view(),
+        name="post-comment-rud",
+    ),
+    path("categories/", PostCategoryListAPIView.as_view(), name="post-category-list"),
     path("<int:post_id>/like/", PostLikeAPIView.as_view(), name="post-like"),
 ]
