69096d27b918179fd8a43ac0e751e8aac3b9a5ae
jhjdl3984 <jhjdl3984@gmail.com>
2026-01-28 14:33:44 +0900

    - ✨ feat     : 쪽지시험 수정 api 구현중

--- apps/exams/serializers/admin_exam_update.py
+++ apps/exams/serializers/admin_exam_update.py
@@ -1,14 +1,27 @@
 from rest_framework import serializers
-from apps.exams.models import ExamDeployment
 
-class AdminExamUpdateSerializer(serializers.ModelSerializer):
+from apps.exams.models import Exam
+from apps.courses.models import Subject
+
+class AdminExamUpdateRequestSerializer(serializers.ModelSerializer):
+    title = serializers.CharField(max_length=50)
+    # 모든 Subject 중, 요청으로 들어온 ID인 Subject 객체를 매핑
+    subject_id = serializers.PrimaryKeyRelatedField(source="subject", queryset=Subject.objects.all())
+    thumbnail_img = serializers.ImageField(required=False)
+
     class Meta:
-        model = ExamDeployment
-        fields = ['title', 'subject_id', 'thumbnail_img']
+        model = Exam
+        fields = ["title", "subject_id", "thumbnail_img"]
 
-    # 409
+    # 409 제목 중복
     def validate_title(self, value):
-        # 이름 중복 체크 예시
-        if ExamDeployment.objects.filter(title=value).exclude(id=self.instance.id).exists():
+        if Exam.objects.filter(title=value).exclude(id=self.instance.id).exists():
             raise serializers.ValidationError("동일한 이름의 쪽지시험이 이미 존재합니다.")
         return value
+
+class AdminExamUpdateResponseSerializer(serializers.ModelSerializer):
+    subject_id = serializers.IntegerField(source="subject.id")
+
+    class Meta:
+        model = Exam
+        fields = ["id", "title", "subject_id", "thumbnail_img_url"]
