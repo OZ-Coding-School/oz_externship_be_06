70f9e3eba0b7f178b0428e6882f8f4cb0661e6cb
god-gy <god.gy0321@gmail.com>
2026-01-28 14:08:19 +0900

    🐛 fix      : develop 이슈용 커밋

--- apps/exams/views/admin_exam_deployment_detail_views.py
+++ apps/exams/views/admin_exam_deployment_detail_views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.urls import reverse
 from drf_spectacular.utils import OpenApiResponse, extend_schema
 from rest_framework import status
@@ -45,6 +46,41 @@ class AdminExamDeploymentDetailAPIView(APIView):
                 {"error_detail": ErrorMessages.INVALID_DEPLOYMENT_DETAIL_REQUEST.value},
                 status=status.HTTP_400_BAD_REQUEST,
             )
+
+        if settings.USE_EXAM_MOCK:
+            access_url = request.build_absolute_uri(reverse("exams:take-exam", kwargs={"deployment_id": deployment_id}))
+            payload = {
+                "id": deployment_id,
+                "exam_access_url": access_url,
+                "access_code": "A7Bd9K",
+                "cohort": {
+                    "id": 11,
+                    "number": 11,
+                    "display": "백엔드 11기",
+                    "course": {
+                        "id": 12,
+                        "name": "백엔드",
+                        "tag": "BE",
+                    },
+                },
+                "submit_count": 58,
+                "not_submitted_count": 12,
+                "duration_time": 45,
+                "open_at": "2025-03-02 10:00:00",
+                "close_at": "2025-03-02 12:00:00",
+                "created_at": "2025-03-01 14:20:33",
+                "exam": {
+                    "id": 101,
+                    "title": "Python 기본 문법 테스트",
+                    "thumbnail_img_url": "https://img.com/images/sample.png",
+                },
+                "subject": {
+                    "id": 32,
+                    "name": "Python",
+                },
+            }
+            serializer = self.serializer_class(payload)
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
         try:
             payload = get_exam_deployment_detail(deployment_id)
