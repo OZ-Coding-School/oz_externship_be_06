f98dc012d0c06ebdf08405a5f93a590e05e57fe0
jhjdl3984 <jhjdl3984@gmail.com>
2026-01-28 16:20:48 +0900

    - ✨ feat     : 쪽지시험 어드민 수정 api 구현중

--- apps/exams/services/admin_exam_update_service.py
+++ apps/exams/services/admin_exam_update_service.py
@@ -1,11 +1,9 @@
 from django.db import transaction
-from django.shortcuts import get_object_or_404
 
 from apps.exams.models import Exam
 from apps.exams.exceptions import (
     ExamNotFoundError,
 )
-from apps.core.utils.s3 import S3Uploader
 from apps.courses.models import Subject
 
 
@@ -15,10 +13,11 @@ def update_exam(
     exam_id: int,
     title: str,
     subject: Subject,   # serializer가 조회한 subject_id의 인스턴스
-    thumbnail_img=None,
+    thumbnail_img_url: str | None = None,
 ) -> Exam:
 
-    #  시험 조회 (404)
+    # 시험 조회 (404)
+    # 한 시험을 2명이 동시에 수정할 때, 한명이 끝날 때까지 대기
     try:
         exam = Exam.objects.select_for_update().get(id=exam_id)
     except Exam.DoesNotExist:
@@ -28,14 +27,9 @@ def update_exam(
     exam.title = title
     exam.subject = subject
 
-    # 썸네일 수정 (있는 경우만)
-    if thumbnail_img:
-        uploader = S3Uploader()
-        thumbnail_url = uploader.upload(
-            file=thumbnail_img,
-            folder="exams"
-        )
-        exam.thumbnail_img_url = thumbnail_url
+    # 썸네일 처리
+    if thumbnail_img_url is not None:
+        exam.thumbnail_img_url = thumbnail_img_url
 
     # 저장
     exam.save()
