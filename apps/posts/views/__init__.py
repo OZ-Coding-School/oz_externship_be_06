088b276dcbb34604b2f7219a7c4c07949193d699
huijeong99 <hwayuya@gmail.com>
2026-01-28 14:01:23 +0900

    Chore: clean up imports and formatting in posts/views/__init__.py

--- apps/posts/views/__init__.py
+++ apps/posts/views/__init__.py
@@ -1,18 +1,14 @@
+from .post_category_views import PostCategoryListAPIView
 from .post_comment_views import (
     PostCommentListCreateAPIView,
     PostCommentRetrieveUpdateDestroyAPIView,
 )
 
-# Backward/legacy import name: point to the same view implementation.
 PostCommentListAPIView = PostCommentListCreateAPIView
-
-from .post_category_views import PostCategoryListAPIView
 
 __all__ = [
     "PostCommentListCreateAPIView",
     "PostCommentRetrieveUpdateDestroyAPIView",
-from .post_category_views import PostCategoryListAPIView
-
-__all__ = [
+    "PostCommentListAPIView",
     "PostCategoryListAPIView",
 ]
