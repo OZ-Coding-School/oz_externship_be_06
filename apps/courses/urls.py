c06c025482506168abbe2e1863ff450f89634da6
2joonkim <joon271694@gmail.com>
2026-01-29 11:19:01 +0900

    🎨 style: 불필요한 주석 삭제

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -6,6 +6,6 @@ from apps.courses.views.course_view import CourseListAPIView
 urlpatterns = [
     # 기존 과정 목록 조회
     path("course/", CourseListAPIView.as_view(), name="course-list"),
-    # 110: 기수 리스트 조회 (일반 사용자)
+
     path("<int:course_id>/cohorts", CohortListView.as_view(), name="cohort-list"),
 ]
