525553b6130d3e2f8e4f8591e104ac2d78d00e10
2joonkim <joon271694@gmail.com>
2026-01-29 11:22:25 +0900

    🎨 style: 코드 포맷팅

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -6,6 +6,5 @@ from apps.courses.views.course_view import CourseListAPIView
 urlpatterns = [
     # 기존 과정 목록 조회
     path("course/", CourseListAPIView.as_view(), name="course-list"),
-
     path("<int:course_id>/cohorts", CohortListView.as_view(), name="cohort-list"),
 ]
