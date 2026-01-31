8f4c6acb022c2d8f30dd9c119e027583c5cf57ba
dskim6797 <dskim6797@gmail.com>
2026-02-01 06:50:24 +0900

    🐛 fix: 인수 유형 에러 수정

--- apps/qna/views/answer_view.py
+++ apps/qna/views/answer_view.py
@@ -1,6 +1,6 @@
 from typing import cast
 
-from drf_spectacular.utils import OpenApiExample, OpenApiResponse, extend_schema
+from drf_spectacular.utils import OpenApiResponse, extend_schema
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
@@ -36,32 +36,32 @@ class AnswerCreateAPIView(QnaBaseAPIView):
         summary="답변 등록 API",
         description=ApiDescriptions.ANSWER_CREATE,
         request=ser_ans_reqs.AnswerCreateSerializer,
-        examples=RequestBodyExamples.ANSWER_CREATE,
+        examples=[RequestBodyExamples.ANSWER_CREATE],
         responses={
             201: OpenApiResponse(
                 description="답변 등록 성공",
                 response=ser_ans_rep.AnswerCreateResponseSerializer,
-                examples=SuccessResponseExamples.ANSWER_CREATE,
+                examples=[SuccessResponseExamples.ANSWER_CREATE],
             ),
             400: OpenApiResponse(
                 description="Bad Request",
                 response=dict,
-                examples=ErrorResponseExamples.ANSWER_CREATE_400,
+                examples=[ErrorResponseExamples.ANSWER_CREATE_400],
             ),
             401: OpenApiResponse(
                 description="Unauthorized",
                 response=dict,
-                examples=ErrorResponseExamples.ANSWER_CREATE_401,
+                examples=[ErrorResponseExamples.ANSWER_CREATE_401],
             ),
             403: OpenApiResponse(
                 description="Forbidden",
                 response=dict,
-                examples=ErrorResponseExamples.ANSWER_CREATE_403,
+                examples=[ErrorResponseExamples.ANSWER_CREATE_403],
             ),
             404: OpenApiResponse(
                 description="Not Found",
                 response=dict,
-                examples=ErrorResponseExamples.ANSWER_CREATE_404,
+                examples=[ErrorResponseExamples.ANSWER_CREATE_404],
             ),
         },
         tags=["qna"],
