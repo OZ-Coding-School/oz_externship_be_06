6ce6d77887f9bec9ed923311246b05454c96be8a
dskim6797 <dskim6797@gmail.com>
2026-02-01 09:33:59 +0900

    💡 chore: urls 수정

---
+++ apps/qna/urls/qna_urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from apps.qna.views.answer_view import AnswerCreateAPIView
+from apps.qna.views.question_view import (
+    QuestionCreateListAPIView,
+    QuestionDetailAPIView,
+)
+
+urlpatterns = [
+    # --- QnA URL Endpoints ---
+    path("questions", QuestionCreateListAPIView.as_view(), name="question-list-create"),
+    path("questions/<int:question_id>", QuestionDetailAPIView.as_view(), name="question-detail"),
+    path("questions/<int:question_id>/answers", AnswerCreateAPIView.as_view(), name="answer-create"),
+]
