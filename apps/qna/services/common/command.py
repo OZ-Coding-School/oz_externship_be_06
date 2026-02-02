af7db3f66bc41fc51a7d7ca86ab17bc6efb8d1b0
dskim6797 <dskim6797@gmail.com>
2026-02-02 16:45:17 +0900

    ♻️ refactor: domain, path 하드코딩 --> Enum 값으로 리팩터링

--- apps/qna/services/common/command.py
+++ apps/qna/services/common/command.py
@@ -1,8 +1,27 @@
-from typing import Dict
+from enum import Enum
+from typing import Dict, Optional
 
-from apps.qna.exceptions.base_e import QnaBaseException
-from apps.qna.utils.constants import ErrorMessages
 from apps.qna.utils.s3_utils import S3Handler
+
+
+class BucketPath(Enum):
+    """
+    이미지 업로드 도메인 및 S3 경로 정의 Enum
+    """
+
+    QUESTION = ("question", "uploads/images/questions")
+    ANSWER = ("answer", "uploads/images/answers")
+
+    def __init__(self, domain: str, s3_path: str):
+        self.domain = domain
+        self.s3_path = s3_path
+
+    @classmethod
+    def domain_to_s3path(cls, domain: str) -> Optional[str]:
+        """입력받은 문자열 라벨에 해당하는 Enum 멤버 반환"""
+        for target in cls:
+            if target.domain == domain.lower():
+                return target.s3_path
 
 
 class PresignedUrlCommandService:
@@ -10,23 +29,14 @@ class PresignedUrlCommandService:
     공통 이미지 업로드 URL 발급 서비스
     """
 
-    # 도메인별 S3 저장 경로 정의
-    FOLDER_MAP = {
-        "question": "uploads/images/questions",
-        "answer": "uploads/images/answers",
-    }
-
     @classmethod
     def get_presigned_url(cls, domain: str, file_name: str) -> Dict[str, str]:
         """도메인(질문/답변)에 따라 경로를 결정하여 URL 발급"""
-        folder_path = cls.FOLDER_MAP.get(domain)
-        if not folder_path:
-            raise QnaBaseException(detail=ErrorMessages.INVALID_UPLOAD_DOMAIN)
-
+        s3_path = BucketPath.domain_to_s3path(domain)
         s3_handler = S3Handler()
-        result = s3_handler.generate_presigned_put_url(folder_path, file_name)
-
-        if not result:
-            raise QnaBaseException(detail=ErrorMessages.S3_CONNECTION_ERROR)
+        result = s3_handler.generate_presigned_put_url(
+            s3_path,
+            file_name
+        )
 
         return result
