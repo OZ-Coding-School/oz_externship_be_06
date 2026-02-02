ea721a95f2c4898d6b63195cf8aa05400b725348
zodongchan <ejsvkgkarj@gmail.com>
2026-02-02 17:15:08 +0900

    💡 chore    : 기능 추가 없이 코드 수정 (디테일 삭제)

--- apps/users/views/admin/account_views.py
+++ apps/users/views/admin/account_views.py
@@ -11,6 +11,7 @@ from rest_framework.response import Response
 from apps.users.models import User
 from apps.users.serializers.admin.account_serializers import AdminAccountListSerializer
 
+
 class AdminAccountPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = "page_size"
