32cbd07db95380494435ab6e9bb5bf3b1a8a1811
dskim6797 <dskim6797@gmail.com>
2026-02-03 05:47:55 +0900

    ♻️ refactor: QuestionQuerySerializer 이동 (response --> reqeust)

--- apps/qna/serializers/question/request.py
+++ apps/qna/serializers/question/request.py
@@ -21,3 +21,22 @@ class QuestionCreateSerializer(serializers.ModelSerializer[Question]):
     class Meta:
         model = Question
         fields = ["title", "content", "category_id"]
+
+
+# ==============================================================================
+# [GET] Question List
+# /api/v1/qna/questions
+# ==============================================================================
+class QuestionQuerySerializer(serializers.Serializer[Any]):
+    """
+    질문 목록 조회를 위한 쿼리 파라미터 시리얼라이저
+    """
+
+    search_keyword = serializers.CharField(required=False, allow_blank=True)
+    category_id = serializers.IntegerField(required=False)
+    answer_status = serializers.ChoiceField(choices=["waiting", "answered"], required=False)
+    sort = serializers.ChoiceField(choices=["latest", "oldest", "most_views"], default="latest")
+    page = serializers.IntegerField(default=1)
+    size = serializers.IntegerField(default=10)
+
+    default_error_message = ErrorMessages.INVALID_QUESTION_LIST
