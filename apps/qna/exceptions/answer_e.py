a3ebc962d6a1c34e042160e4ad5f789f2e51382e
dskim6797 <dskim6797@gmail.com>
2026-02-04 07:48:33 +0900

    💡 chore: 디폴트값, 변수명 수정 등

--- apps/qna/exceptions/answer_e.py
+++ apps/qna/exceptions/answer_e.py
@@ -6,19 +6,19 @@ from apps.qna.exceptions.base_e import QnaBaseException
 
 class AnswerNotFoundException(QnaBaseException):
     """
-    [404] Not Found(데이터가 존재하지 않음)
+    [404] Not Found (데이터가 존재하지 않음)
     """
 
     status_code = status.HTTP_404_NOT_FOUND
-    default_detail = ErrorMessages.DEFAULT_MESSAGE_404
+    default_detail = ErrorMessages.NOT_FOUND_ANSWER
     default_code = "answer_not_found"
 
 
 class AdoptedAnswerConflictException(QnaBaseException):
     """
-    [409] Conflict(데이터 중복)
+    [409] Conflict (데이터 충돌)
     """
 
     status_code = status.HTTP_409_CONFLICT
-    default_detail = ErrorMessages.DEFAULT_MESSAGE_409
+    default_detail = ErrorMessages.CONFLICT_ANSWER_ADOPT
     default_code = "answer_already_adopted"
