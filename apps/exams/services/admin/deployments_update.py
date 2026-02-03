d314d3555607c2f002e075c154912ace53f09162
“yoon-122” <dudgns1233@gmail.com>
2026-02-03 18:55:08 +0900

    어드민 배포 수정 API 구현

---
+++ apps/exams/services/admin/deployments_update.py
@@ -0,0 +1,19 @@
+from apps.exams.models import ExamDeployment
+
+
+class ExamDeploymentUpdateNotFoundError(Exception):
+    """수정할 배포 정보를 찾지 못했을 때 발생."""
+
+
+def update_exam_deployment(deployment_id: int, validated_data: dict) -> ExamDeployment:
+    try:
+        deployment = ExamDeployment.objects.get(id=deployment_id)
+    except ExamDeployment.DoesNotExist as exc:
+        raise ExamDeploymentUpdateNotFoundError from exc
+
+    deployment.open_at = validated_data["open_at"]
+    deployment.close_at = validated_data["close_at"]
+    deployment.duration_time = validated_data["duration_time"]
+    deployment.save(update_fields=["open_at", "close_at", "duration_time", "updated_at"])
+
+    return deployment
