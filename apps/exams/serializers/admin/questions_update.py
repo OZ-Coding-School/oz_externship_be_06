035be885f35985e74582f0068785709f6852e9ec
jhjdl3984 <jhjdl3984@gmail.com>
2026-02-04 11:06:57 +0900

    ♻️ refactor : question delete와 put 리팩터링
    2. 변경 또는 추가사항
    -question delete와 update 리팩터링

--- apps/exams/serializers/admin/questions_update.py
+++ apps/exams/serializers/admin/questions_update.py
@@ -47,6 +47,7 @@ class AdminExamQuestionUpdateRequestSerializer(serializers.ModelSerializer[ExamQ
         return ret
 
 
+
 class AdminExamQuestionUpdateResponseSerializer(serializers.ModelSerializer[ExamQuestion]):
     question_id = serializers.IntegerField(source="id")
     options = serializers.SerializerMethodField()
