12fb9c50c452ef2000d72b0a249fea47051dc9e5
god-gy <god.gy0321@gmail.com>
2026-02-06 14:33:53 +0900

    ♻️ refactor: exams URL trailing slash 제거

--- apps/exams/urls/admin.py
+++ apps/exams/urls/admin.py
@@ -18,35 +18,35 @@ from apps.exams.views.admin.submissions_detail import AdminExamSubmissionDetailA
 from apps.exams.views.admin.submissions_list import AdminExamSubmissionListAPIView
 
 urlpatterns = [
-    path("exams/<int:exam_id>/questions/", AdminExamQuestionCreateAPIView.as_view(), name="admin-exam-question-create"),
+    path("exams/<int:exam_id>/questions", AdminExamQuestionCreateAPIView.as_view(), name="admin-exam-question-create"),
     # put + delete
-    path("exams/<int:exam_id>/", AdminExamDetailAPIView.as_view(), name="admin-exam-detail"),
+    path("exams/<int:exam_id>", AdminExamDetailAPIView.as_view(), name="admin-exam-detail"),
     path("exams", AdminExamRouterAPIView.as_view(), name="admin-exams"),
     path(
-        "exams/presigned-url/thumbnail/",
+        "exams/presigned-url/thumbnail",
         ExamPresignedUrlAPIView.as_view(),
         name="exam-presigned-url",
     ),
-    path("submissions/", AdminExamSubmissionListAPIView.as_view(), name="admin-exam-submission-list"),
+    path("submissions", AdminExamSubmissionListAPIView.as_view(), name="admin-exam-submission-list"),
     path(
-        "exams/questions/<int:question_id>/",
+        "exams/questions/<int:question_id>",
         AdminExamQuestionDetailAPIView.as_view(),
         name="admin-exam-question-detail",
     ),
-    path("exams/deployments/", AdminExamDeploymentRouterAPIView.as_view(), name="admin-exam-deployments"),
     # detail + delete
+    path("exams/deployments", AdminExamDeploymentRouterAPIView.as_view(), name="admin-exam-deployments"),
     path(
-        "exams/deployments/<int:deployment_id>/",
+        "exams/deployments/<int:deployment_id>",
         AdminExamDeploymentDetailAPIView.as_view(),
         name="admin-exam-deployment-detail",
     ),
     path(
-        "exams/deployments/<int:deployment_id>/status/",
+        "exams/deployments/<int:deployment_id>/status",
         AdminExamDeploymentStatusAPIView.as_view(),
         name="admin-exam-deployment-status",
     ),
     path(
-        "exams/submissions/<int:submission_id>/",
+        "exams/submissions/<int:submission_id>",
         AdminExamSubmissionDetailAPIView.as_view(),
         name="admin-exam-submission-detail",
     ),
